fe8f7bb3526b99167f058bff37406ecc8cb6f244
liukai <liukai0293@126.com>
2015-12-21 18:06:20 +0800

    添加注释

--- src/main/scala/com/sosnoski/concur/article5scala/Hello1.scala
+++ src/main/scala/com/sosnoski/concur/article5scala/Hello1.scala
@@ -1,20 +1,26 @@
 package com.sosnoski.concur.article5scala
 
 import akka.actor._
-import akka.util._
 
 /** Simple hello from an actor in Scala. */
 object Hello1 extends App {
-  
+
   val system = ActorSystem("actor-demo-scala")
-  val hello = system.actorOf(Props[Hello])
-  hello ! "Bob"
+  //创建actor系统
+  //Props 中为创建Actor需要的配置信息
+  //或者Props(classOf[Hello])传递的是Hello的构造方法的调用
+  val hello = system.actorOf(Props[Hello]) //在系统内创建actor，返回一个actor引用
+
+  hello ! "Bob" //使用actor的引用向actor发送消息 同 tell()方法
   Thread sleep 1000
+  //关闭actor系统
   system shutdown
-  
+
   class Hello extends Actor {
     def receive = {
+      //receive方法实现了接受消息处理的方式
       case name: String => println(s"Hello $name")
     }
   }
+
 }
--- src/main/scala/com/sosnoski/concur/article5scala/Hello3.scala
+++ src/main/scala/com/sosnoski/concur/article5scala/Hello3.scala
@@ -1,33 +1,49 @@
 package com.sosnoski.concur.article5scala
 
 import akka.actor._
-import akka.util._
 
 /** Hellos with properties passed to constructor, using messages to communicate. */
 object Hello3 extends App {
 
   import Greeter._
+
   val system = ActorSystem("actor-demo-scala")
-  val bob = system.actorOf(props("Bob", "Howya doing"))
+  private val of: ActorRef = system.actorOf(props("Bob", "Howya doing"))
+  val bob = of
   val alice = system.actorOf(props("Alice", "Happy to meet you"))
   bob ! Greet(alice)
-  alice ! Greet(bob)
+  println("alice"+alice.hashCode())
+  println("bob"+bob.hashCode())
+
+
+  //alice ! Greet(bob)
   Thread sleep 1000
   system shutdown
 
   object Greeter {
+
     case class Greet(peer: ActorRef)
+
     case object AskName
+
     case class TellName(name: String)
+
     def props(name: String, greeting: String) = Props(new Greeter(name, greeting))
   }
 
   class Greeter(myName: String, greeting: String) extends Actor {
+
     import Greeter._
+
     def receive = {
       case Greet(peer) => peer ! AskName
-      case AskName => sender ! TellName(myName)
+      //发送给 actor 的每个消息都包含由 Akka 提供的一些附加信息，最特别的是消息发送方的 ActorRef。
+      // 您可在消息处理过程中的任何时刻，通过调用在 actor 基类上定义的 sender() 方法
+      // 来访问这些发送方的信息。Greeter actor 在处理 AskName 消息的过程中会使用发送方引用，
+      // 以便将 TellName 响应发送给正确的 actor。
+      case AskName => sender ! TellName(myName); println(sender.hashCode()) //sender是AskName的发送方
       case TellName(name) => println(s"$greeting, $name")
     }
   }
+
 }
